b5da260c393c53fdcc8eff6ac07900472746b36a
Sardor <sadriddinov.sl.0906@gmail.com>
2026-06-07 04:35:43 +0500

    Update database.py

--- database.py
+++ database.py
@@ -8,11 +8,15 @@ pool = None
 
 async def init_db():
     global pool
+    db_url = os.getenv("DATABASE_URL")
+    print(f"DATABASE_URL = {db_url}")  # покажет в логах что читается
+    if not db_url:
+        raise Exception("DATABASE_URL is not set!")
     pool = await asyncpg.create_pool(
-        os.getenv("Render"),
+        db_url,
         min_size=2,
         max_size=10,
-        statement_cache_size=0  # обязательно для Supabase pooler
+        statement_cache_size=0
     )
 
 async def get_pool():
